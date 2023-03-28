c49a8d19685d22d84b7b113f0160c10fbc84af9f
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-03-28 20:44:38 -0300

    salvando

---
+++ mainjQ.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('header button').click( function(){
+    $('form').slideDown();
+  })
+  $("#botao-cancelar").click(function(){
+    $("form").slideUp();
+  })
+
+  $('Form').on('submit', function(e){
+    e.preventDefault();
+    const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+    const novoItem = $('<li style="display: none"></li>');
+    $(`<img src=" ${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+    $(`
+    <div class="imagen-ink">
+
+    <a href="${enderecoDaNovaImagem}" title="Ver imagem em tamanho real" target="_blank">
+    Ver imagem em tamanho real
+    </a>
+  </div>`).appendTo(novoItem);
+  $(novoItem).appendTo('ul');
+  $(novoItem).fadeIn(1000);
+  $('#endereco-imagem-nova').val('');
+  })
+
+})
